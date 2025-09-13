248998b9908a558476e2466cd9288bc1d3b525a6
Filip Swierczynski <swirek.gw@hotmail.com>
2025-09-13 10:48:15 +0200

    techniques added

---
+++ BJJ-Drill/src/app/technique/technique.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technique',
+  standalone: true,
+  imports: [],
+  templateUrl: './technique.html',
+  styleUrl: './technique.scss'
+})
+export class Technique {
+
+}
